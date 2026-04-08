0d4e39d7a2574ec582a0eca054e5fd4f33e0cf0e
Jayanga Rathna Pradeepa <jayanga.pradeepa@healthya.co.uk>
2026-04-08 20:41:11 +0530

    Remove DEFAULT_SCHEMA_CLASS from REST_FRAMEWORK settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -153,7 +153,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
-    "DEFAULT_SCHEMA_CLASS": "rest_framework.openapi.AutoSchema",
 }
 
 # ─────────────────────────────────────────────────────────────────────────────
